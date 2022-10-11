3f57054905456da81635cbc0e05dac5caed5a795
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-12 02:00:05 +0600

    quiz

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Nav from './components/Nav/Nav';
 
 
 function App() {
-  // useEffect( ()=> {
-  //   fetch('https://openapi.programming-hero.com/api/quiz')
-  //   .then(res => res.json())
-  //   .then(data => console.log(data))
-  // } ,[])
+  
   return (
       <div>
         <Nav></Nav>
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,27 +1,62 @@
 import { faCircle, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-const Question = ({ question }) => {
-  const { options } = question;
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const Question = (props) => {
+  const { options } = props.question;
+  let [index, setIndex] = useState(undefined);
+  console.log(props.question.correctAnswer);
+  const notify = (text) => toast(text);
   return (
     <div className="border-orange-400 border-2 m-5">
       <div className="header flex justify-between">
         <div className="question">
-          <h2> {question.question}</h2>
+          <h2>
+            <span className="text-green-500">Question:</span>
+            {props.question.question}
+          </h2>
         </div>
-        <div className="eye">
+        <div
+          className="eye cursor-pointer"
+          onClick={() => notify(props.question.correctAnswer)}
+        >
           {<FontAwesomeIcon icon={faEye} className="text-2xl" />}
         </div>
       </div>
       {options.map((element) => {
         return (
           <div className="flex">
-            <div className="circle">
+            <div
+              className={`circle ${
+                (console.log("options[index]= " + options[index]),
+                console.log("element=" + element),
+                options[index] === element
+                  ? console.log("true")
+                  : console.log("false"),
+                options[index] === element ? "text-lime-400	" : "text-zinc-900")
+              }`}
+            >
               <FontAwesomeIcon icon={faCircle} />
             </div>
-            <div className="option">
+            <div
+              className="option cursor-pointer"
+              onClick={(e) => {
+                const i = options.indexOf(e.target.textContent);
+                // console.log(e.target.textContent);
+                // console.dir(e.target);
+                // console.log("indexclick= " + i);
+                setIndex(i);
+                const ans =
+                  e.target.textContent === props.question.correctAnswer;
+                ans
+                  ? notify("Your Answer is Right")
+                  : notify('"Your Answer is Wrong"');
+              }}
+            >
               <h3>{element}</h3>
+              <ToastContainer />
             </div>
           </div>
         );
